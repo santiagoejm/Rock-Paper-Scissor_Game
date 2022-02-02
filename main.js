107d36ebd79b95ed5b64307ae278f1e331472a01
Santiago Julian <sjulian983@gmail.com>
2022-02-02 19:48:30 +0100

    version 1.0

---
+++ main.js
@@ -0,0 +1,72 @@
+const playerMove = document.querySelector("#player-move");
+const computerMove = document.querySelector("#computer-move");
+const playerScore = document.querySelector("#player-score");
+const computerScore = document.querySelector("#computer-score");
+const resultDisplay = document.querySelector("#result");
+const button = document.getElementsByTagName("button");
+
+// const rock = document.getElementById("rock");
+// const paper = document.querySelector("paper");
+// const scissor = document.querySelector("scissor");
+
+let player;
+let pc;
+let result;
+let playScore = 0;
+let pcScore = 0;
+
+const clickHandler = (e) => {
+  player = e.target.id;
+  playerMove.textContent = player;
+  pcMove();
+  getResult(player, pc);
+  updateScore();
+};
+
+const pcMove = () => {
+  random = Math.floor(Math.random() * 3);
+  if (random == 0) {
+    pc = "rock";
+  } else if (random == 1) {
+    pc = "paper";
+  } else {
+    pc = "scissor";
+  }
+  computerMove.textContent = pc;
+};
+
+const getResult = (player, pc) => {
+  switch (`${player}${pc}`) {
+    case "rockrock":
+    case "paperpaper":
+    case "scissorscissor":
+      result = "IT'S A DRAW!";
+      break;
+    case "rockpaper":
+    case "paperscissor":
+    case "scissorrock":
+      result = "YOU LOSE!!";
+      break;
+    case "rockscissor":
+    case "paperrock":
+    case "scissorpaper":
+      result = "YOU WIN!!";
+      break;
+  }
+  resultDisplay.textContent = result;
+};
+
+const updateScore = () => {
+  if (result == "YOU LOSE!!") {
+    pcScore++;
+    computerScore.textContent = pcScore;
+  } else if (result == "YOU WIN!!") {
+    playScore++;
+    playerScore.textContent = playScore;
+  }
+};
+
+let btnArray = Array.from(button);
+btnArray.forEach((btn) => {
+  btn.addEventListener("click", clickHandler);
+});
